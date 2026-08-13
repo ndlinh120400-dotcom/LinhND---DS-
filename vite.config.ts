f0fbd3cca55025d79d050fb963a0ac1707e93697
ndlinh120400-dotcom <ndlinh120400@gmail.com>
2026-08-13 11:24:26 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/LinhND---DS-/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
